d083be6c46a99f2a52ee44778bc6243d2569cbfa
hdpuk86 <hayley.d.prior@gmail.com>
2017-12-01 16:10:51 +0000

    adds record & tests

---
+++ specs/record_spec.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var Record = require('../record.js');
+
+describe("Record Tests", function(){
+
+  beforeEach(function(){
+    record = new Record("Blink-182", "Feeling This", "Pop-Punk", 1);
+  });
+
+  it('should have an artist', function(){
+    assert.strictEqual(record.artist, "Blink-182");
+  });
+
+  it('should have a title', function(){
+    assert.strictEqual(record.title, "Feeling This");
+  });
+
+  it('should have a genre', function(){
+    assert.strictEqual(record.genre, "Pop-Punk");
+  });
+
+  it('should have a price', function(){
+    assert.strictEqual(record.price, 1);
+  });
+
+});
